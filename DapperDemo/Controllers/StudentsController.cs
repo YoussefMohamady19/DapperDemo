48c866272d2e1ccdd95ef507735d47282cb0e51f
YoussefMohamady19 <youssefmohamady121@gmail.com>
2025-10-25 16:09:41 +0300

    Add project files.

---
+++ DapperDemo/Controllers/StudentsController.cs
@@ -0,0 +1,48 @@
+﻿using DapperDemo.Models;
+using DapperDemo.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DapperDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : Controller
+    {
+        private readonly StudentRepository _repo;
+        public StudentsController(StudentRepository repo)
+        {
+            _repo=repo;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var students=await _repo.GetAllAsync();
+            return Ok(students);
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var student=await _repo.GetByIdAsync(id);
+            return Ok(student);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(Student student)
+        {
+            await _repo.CreateAsync(student);
+            return Ok("Student Created successfully");
+        }
+        [HttpPut("id")]
+        public async Task<IActionResult> Update(Student student)
+        {
+            await _repo.UpdateAsync(student);
+            return Ok("Student updated successfully");
+        }
+        [HttpDelete("id")]
+        public async Task<IActionResult>Delete(int id)
+        {
+            await _repo.DeleteAsync(id);
+            return Ok("Student deleted successfully");
+        }
+       
+    }
+}
---
+++ DapperDemo/Repositories/StudentRepository.cs
@@ -0,0 +1,50 @@
+﻿using Dapper;
+using DapperDemo.Models;
+using Microsoft.Data.SqlClient;
+using Microsoft.Identity.Client;
+using System.Data;
+
+namespace DapperDemo.Repositories
+{
+    public class StudentRepository
+    {
+        private readonly string _connectionString;
+        public StudentRepository(IConfiguration configuration) 
+        {
+            _connectionString = configuration.GetConnectionString("DefaultConnection");
+        }
+        private IDbConnection CreateConnection()
+            => new SqlConnection(_connectionString);
+        public async Task<IEnumerable<Student>> GetAllAsync()
+        {
+            using var connection = CreateConnection();
+            return await connection.QueryAsync<Student>("Select * from Students");
+        }
+        public async Task<Student?> GetByIdAsync(int id)
+        {
+            using var connection = CreateConnection();
+            return await connection.QueryFirstOrDefaultAsync<Student>(
+                "SELECT * FROM Students WHERE Id = @Id", new { Id = id });
+        }   
+        public async Task<int> CreateAsync(Student student)
+        {
+            using var connection= CreateConnection();
+            var sql = "Insert Into Students (Name, Age, Email) VALUES (@Name, @Age, @Email)";
+            return await connection.ExecuteAsync(sql, student);
+
+        }
+        public async Task<int> UpdateAsync(Student student)
+        {
+            using var connection =CreateConnection();
+            var sql = "Update Students SET Name=@Name,Age=@Age,Email=@Email WHERE Id=@Id";
+            return await connection.ExecuteAsync(sql, student);
+        }
+        public async Task<int> DeleteAsync (int id)
+        {
+            using var connection = CreateConnection();
+            var sql = "Delete From Students Where Id=@Id";
+            return await connection.ExecuteAsync(sql, new { Id = id });
+        }
+
+    }
+}
